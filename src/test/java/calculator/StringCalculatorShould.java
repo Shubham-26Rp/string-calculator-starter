17eb460ce54ffc8588190d76f3c4b7f722fc7e6a
Shubham-26Rp <shubh.shete77@gmail.com>
2021-10-17 14:29:07 +0530

    sixth commit

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,84 +6,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-
 	StringCalculator stringcalculator = new StringCalculator();
 
 	@Test
-	public void returnZeroOnEmptyString()
-	{
+	public void returnZeroOnEmptyString() {
 		assertEquals(0, stringcalculator.add(""));
 	}
-	
+
 	@Test
-	public void returnNumOnSingleArg()
-	{
+	public void returnNumOnSingleArg() {
 		assertEquals(1, stringcalculator.add("1"));
 	}
-	
+
 	@Test
-	public void returnSumOnTwoArgsDelimitedByComma()
-	{
+	public void returnSumOnTwoArgsDelimitedByComma() {
 		assertEquals(3, stringcalculator.add("1,2"));
 	}
-	
+
 	@Test
-	public void returnSumOnMultipleArgsDelimitedByComma()
-	{
+	public void returnSumOnMultipleArgsDelimitedByComma() {
 		assertEquals(6, stringcalculator.add("1,2,3"));
 	}
 
 	@Test
-	public void acceptNewlineAsValidDelimiter()
-	{
+	public void acceptNewlineAsValidDelimiter() {
 		assertEquals(6, stringcalculator.add("1\n2,3"));
 	}
-	
+
 	@Test
-	public void customDelimeter()
-	{
+	public void customDelimeter() {
 		assertEquals(3, stringcalculator.add("//;\n1;2"));
 	}
-	
+
 	@Test
-	public void raiseExceptionOnNegativeNumbers()
-	{
+	public void raiseExceptionOnNegativeNumbers() {
 		try {
 			stringcalculator.add("-1,-2,3");
 			fail("Exception expected.");
 		}
-		
-		catch(RuntimeException e)
-		{
+
+		catch (RuntimeException e) {
 			assertEquals("Negatives not allowed: [-1, -2]", e.getMessage());
 		}
 	}
 
-	 @Test
-		public void validateCallCount()
-		{
-			assertEquals(StringCalculator.getCallCount(), StringCalculator.count);
-		}
-	  
-	 @Test
-		public void ignoreNumsGrtThn1000()
-		{
-			assertEquals(2, stringcalculator.add("2,1001"));
-		}
-	 @Test
-		public void validateAnyLengthDelimeter()
-		{
-			assertEquals(6, stringcalculator.add("//[***]\n1***2***3"));
-		}
-	    
-	       @Test
-			public void multipleDelimeter()
-			{
-				assertEquals(6, stringcalculator.add("//[*][%]\n1*2%3"));
-			}
-	       @Test
-			public void mulCustomDelWithAnyLength()
-			{
-				assertEquals(6, stringcalculator.add("//[**][%%]\n1**2%%3"));
-			}
+	@Test
+	public void validateCallCount() {
+		assertEquals(StringCalculator.getCallCount(), StringCalculator.count);
+	}
+
+	@Test
+	public void ignoreNumsGrtThn1000() {
+		assertEquals(2, stringcalculator.add("2,1001"));
+	}
+
+	@Test
+	public void validateAnyLengthDelimeter() {
+		assertEquals(6, stringcalculator.add("//[***]\n1***2***3"));
+	}
+
+	@Test
+	public void multipleDelimeter() {
+		assertEquals(6, stringcalculator.add("//[*][%]\n1*2%3"));
+	}
+
+	
 }
