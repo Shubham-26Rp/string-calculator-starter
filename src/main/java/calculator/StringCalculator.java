d2d7d3bc271a11ab3cae6990eb2e811c66db049d
Shubham-26Rp <shubh.shete77@gmail.com>
2021-10-17 10:58:50 +0530

    second commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,17 @@
 package calculator;
 
 class StringCalculator {
+	static int count = 0;
+	
+    public int add(String numbers) {
+    	synchronized(this)
+		{
+			count++;
+		}
+		
+		if(numbers.isEmpty())
+			return 0;
+		return 0;
+		
 
-    public int add(String input) {
-        return 0;
-    }
-
-}
+}}
